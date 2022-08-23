432534288565cc0fe46657f45a0d7ae886b195ef
Armstrong Lohãns <lohansdemelo1108@gmail.com>
2022-08-23 15:32:02 -0300

    Add Middleware

--- bin/enzitech_backend.dart
+++ bin/enzitech_backend.dart
@@ -4,13 +4,29 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 
 Future<void> main(List<String> arguments) async {
+  var pipeline = Pipeline().addMiddleware(log());
+
   final server = await io.serve(
-    _handler,
+    pipeline.addHandler(_handler),
     '0.0.0.0',
     4466,
   );
 
   print('Online - ${server.address.address}:${server.port}');
+}
+
+Middleware log() {
+  return (handler) {
+    return (request) async {
+      // Antes de executar
+      print('Solicitado: ${request.url}');
+      var response = await handler(request);
+
+      // Depois de executar
+      print('[${response.statusCode}] - Response');
+      return response;
+    };
+  };
 }
 
 FutureOr<Response> _handler(Request request) {
